f47a150a0e6de2599153e1ea1bcacf59e7098cf3
Deyvid Lopes <deyvidyury@yahoo.com.br>
2020-05-20 22:21:05 -0300

    Making request to query service

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -6,14 +6,14 @@ import CommentList from "./CommentList";
 function PostList() {
   const [posts, setPosts] = useState({});
 
-  const feetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4002/posts");
 
     setPosts(res.data);
   };
 
   useEffect(() => {
-    feetchPosts();
+    fetchPosts();
   }, []);
 
   const renderedPosts = Object.values(posts).map((post) => {
@@ -25,7 +25,7 @@ function PostList() {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
